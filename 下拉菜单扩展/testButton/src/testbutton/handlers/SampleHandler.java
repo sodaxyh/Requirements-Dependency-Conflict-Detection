f10b885e586ed0a3621999e630da1d02e8045520
sodaxyh <tkd.girl@163.com>
2022-10-21 22:12:21 +0800

    Add files via upload

---
+++ 下拉菜单扩展/testButton/src/testbutton/handlers/SampleHandler.java
@@ -0,0 +1,34 @@
+package testbutton.handlers;
+
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class SampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+//		try {
+//			Process pr = Runtime.getRuntime().exec("python E://Eclipse/runtime-New_configuration(4)/req.sample/src/Run_client.py");
+//			pr.waitFor();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Automatic Analysis",
+				"Finished");
+		return null;
+	}
+}
